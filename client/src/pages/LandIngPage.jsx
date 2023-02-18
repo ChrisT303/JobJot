8039d23025d06aef3815b31d31dcf36c0022ad9c
deftonechris <deftonechris@msn.com>
2023-02-17 17:39:56 -0700

    one minor spacing issue between logo and text

--- client/src/pages/LandIngPage.jsx
+++ client/src/pages/LandIngPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import JobJotLogo from '../assets/images/JobJotLogo.svg'
+import JobJotLogo2 from '../assets/images/JobJotLogo2.svg'
 import mainImg from '../assets/images/mainLandingPageImg.svg'
 
 
@@ -7,7 +8,7 @@ const LandingPage = () => {
   return (
     <main>LandingPage
         <nav>
-         <img src={JobJotLogo} alt="JobJot Logo" className='logo'/>
+         <img src={JobJotLogo2} alt="JobJot Logo" className='logo'/>
         </nav>
     </main>
   )
