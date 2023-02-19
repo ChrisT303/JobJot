e29dd1e8953864644553ea52904a3f3ac3aee368
deftonechris <deftonechris@msn.com>
2023-02-18 19:17:35 -0700

    error page done

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold mb-6">404 - Page Not Found</h1>
+      <p className="text-lg mb-6">The page you are looking for does not exist.</p>
+      <Link to="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Go to Homepage
+      </Link>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
+
