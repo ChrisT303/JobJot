125bc4c263bec30a4bd5a2c077cb2964ff28fb77
deftonechris <deftonechris@msn.com>
2023-02-19 18:24:25 -0700

    quick font change

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
 };
+
