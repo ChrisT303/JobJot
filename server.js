649e9defe9e482444a44743947220d4fe9a7b655
deftonechris <deftonechris@msn.com>
2023-02-20 17:21:50 -0700

    server set up and connection to mongo

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/connection.js";
+
+import notFoundMiddleware from "./middleware/notFound.js";
+import errorHandlerMiddleware from "./middleware/error.js";
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 4000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
