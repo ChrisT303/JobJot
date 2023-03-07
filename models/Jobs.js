e2b5fca4e66c99ef8608566991b762c99bd5d7d9
deftonechris <deftonechris@msn.com>
2023-03-07 12:21:29 -0700

    job model

---
+++ models/Jobs.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, "Please provide company name"],
+  },
+  position: {
+    type: String,
+    required: [true, "Please provide company name"],
+  },
+  status: {
+    type: String,
+    enum: ["Applied", "Interview", "Offer", "Rejected"],
+    default: "Applied",
+  },
+  jobType: {
+    type: String,
+    enum: ["Full Time", "Part Time", "Contract", "Internship", "Remote"],
+    default: "Full Time",
+  },
+  jobLocation: {
+    type: String,
+    default: "My Location",
+    required: true,
+  },
+
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "Please provide user id"],
+  },
+}, { timestamps: true });
+
+
+export default mongoose.model("Job", JobSchema);
