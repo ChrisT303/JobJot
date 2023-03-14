ab815f545c760692705b52f9bcdf2febb43031aa
deftonechris <deftonechris@msn.com>
2023-03-13 20:10:29 -0600

    cards for stats done, place holder colors for now

--- client/src/components/StatItem.jsx
+++ client/src/components/StatItem.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const StatItem = () => {
+const StatItem = ({ title, count, icon, color, bcg }) => {
   return (
-    <div>
-    <h1>StatItem</h1>
-    </div>
-  )
-}
+<div className={`border rounded-md p-4 flex flex-col items-center justify-between ${color}`} style={{backgroundColor: bcg}}>
+  <header className="flex items-center">
+    <span className="text-3xl font-bold mr-8">{count}</span>
+    <div className="text-3xl">{icon}</div>
+  </header>
+  <h5 className="mt-4 text-lg font-bold">{title}</h5>
+</div>
 
-export default StatItem
+
+  );
+};
+
+export default StatItem;
+
+
+
+  
+
+
+
+
+
--- client/src/components/StatsCont.jsx
+++ client/src/components/StatsCont.jsx
@@ -1,50 +1,55 @@
-import React from 'react'
-import { useGlobalContext } from '../context/context'
-import StatItem from './StatItem'
+import React from "react";
+import { useGlobalContext } from "../context/context";
+import StatItem from "./StatItem";
 import { MdPendingActions } from "react-icons/md";
-import { FaPeopleArrows, FaRegHandshake} from "react-icons/fa";
+import { FaPeopleArrows, FaRegHandshake } from "react-icons/fa";
 import { BiNoEntry } from "react-icons/bi";
 
 const StatsCont = () => {
-  const {stats} = useGlobalContext()
+  const { stats } = useGlobalContext();
   const defaultStats = [
     {
       title: "Applied",
       count: stats.Applied || 0,
-      icon: <MdPendingActions />,
-      color: '#e9b949',
-      bcg: '#fcefc7'
+      icon: <MdPendingActions className="yellow" />,
+      color: "yellow",
+      bcg: "#fcefc7"
     },
     {
       title: "Interview",
       count: stats.Interview || 0,
-      icon: <FaPeopleArrows />,
-      color: '#647acb',
-      bcg: '#e0e8f9'
+      icon: <FaPeopleArrows className="blue" />,
+      color: "blue",
+      bcg: "#e0e8f9"
     },
     {
       title: "Offer",
       count: stats.Offer || 0,
-      icon: <FaRegHandshake />,
-      color: '#e9b949',
-      bcg: '#f9e8d2'
+      icon: <FaRegHandshake className="yellow" />,
+      color: "yellow",
+      bcg: "#f9e8d2"
     },
     {
       title: "Declined",
-      count: stats.Declined|| 0,
-      icon: <BiNoEntry />,
-      color: '#e9b949',
+      count: stats.Declined || 0,
+      icon: <BiNoEntry className="red" />,
+      color: "red",
       bcg: "#ffeeee"
-    }
-  ]
+    },
+  ];
+  
+  
 
   return (
-    <div className='grid grid-cols-1 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-4'>
+    <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-4">
       {defaultStats.map((item, index) => {
-        return <StatItem key={index} {...item} />
+        return <StatItem key={index} {...item} />;
       })}
     </div>
-  )
-}
+  );
+};
 
-export default StatsCont
+export default StatsCont;
+
+
+
