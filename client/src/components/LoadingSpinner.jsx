d687bc0fb6ff2cf6a2ccbd55dbe76210cfc9095c
deftonechris <deftonechris@msn.com>
2023-03-17 20:17:34 -0600

    depoly 2

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
-const LoadingSpinner = ({center}) => {
+const LoadingSpinner = ({ center }) => {
   return (
-    <div className={center? 'flex justify-center items-center mx-auto': "flex justify-center items-center"}>
-      <FontAwesomeIcon icon={faCircleNotch} className="animate-spin h-16 w-16 text-blue-500" style={{marginRight: "0.5rem"}} />
-      <span className="sr-only">Loading...</span>
+    <div className={center ? "flex justify-center items-center mx-auto" : "flex justify-center items-center"}>
+      <div className="w-10 h-10 border-4 border-blue-500 rounded-full animate-spin"></div>
     </div>
   );
 };
 
 export default LoadingSpinner;
+
 
